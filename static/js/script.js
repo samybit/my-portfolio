018199c54dafe7228caf3ab5930401b07cb9ffff
samybit <samyb.samir@gmail.com>
2026-01-29 17:16:50 +0200

    Add card hover effect & Fix .ignore files

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const cards = document.querySelectorAll(".card, .ratio");
+
+    cards.forEach((card) => {
+        card.addEventListener("mousemove", (e) => {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            card.style.setProperty("--x", `${x}px`);
+            card.style.setProperty("--y", `${y}px`);
+        });
+    });
+});
