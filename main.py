52786a90853a9574ba6df8cf8020ca9c5bacdd69
samybit <samyb.samir@gmail.com>
2026-01-29 02:31:49 +0200

    Add about and projects pages & their routes

--- main.py
+++ main.py
@@ -9,9 +9,33 @@ app.config["SECRET_KEY"] = "your-secret-key-here"
 bootstrap = Bootstrap5(app)
 
 
+# d8888b.  .d88b.  db    db d888888b d88888b .d8888.
+# 88  `8D .8P  Y8. 88    88 `~~88~~' 88'     88'  YP
+# 88oobY' 88    88 88    88    88    88ooooo `8bo.
+# 88`8b   88    88 88    88    88    88~~~~~   `Y8b.
+# 88 `88. `8b  d8' 88b  d88    88    88.     db   8D
+# 88   YD  `Y88P'  ~Y8888P'    YP    Y88888P `8888Y'
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+@app.route("/contact")
+def contact():
+    # We will build the actual form logic in Step 4
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
